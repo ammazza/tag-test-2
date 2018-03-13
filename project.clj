05d4da6adfcd2feffc3ddc8b54ef6d32768af216
Filippo Ammazzalorso <Filippo.Ammazzalorso@data61.csiro.au>
2018-03-13 14:50:45 +1100

    Improved a bit travis. I will tag this a snapshot release.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject hello-travis "0.5.0"
+(defproject hello-travis "0.6.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
